35d8c01fde3c1f6d27fbfe2ce071f91731f8d971
resikrys <resicris@gmail.com>
2025-11-02 19:25:49 +0100

    initial commit

---
+++ src/main/java/com/beasties/beasties_backend/BeastiesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.beasties.beasties_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeastiesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeastiesBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beasties/beasties_backend/BeastiesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beasties.beasties_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeastiesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
