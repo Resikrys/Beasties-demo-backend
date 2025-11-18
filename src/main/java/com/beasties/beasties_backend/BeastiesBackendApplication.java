fb498464ee7dff3cb98e1c076642cbbbce92d086
resikrys <resicris@gmail.com>
2025-11-18 20:18:34 +0100

    add: enableScheduling annotation in main class

--- src/main/java/com/beasties/beasties_backend/BeastiesBackendApplication.java
+++ src/main/java/com/beasties/beasties_backend/BeastiesBackendApplication.java
@@ -2,8 +2,10 @@ package com.beasties.beasties_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class BeastiesBackendApplication {
 
 	public static void main(String[] args) {
